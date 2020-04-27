107ce5e330d1b50653031df6affbb42c22be1145
hassehulabeck <hansbudandersson@gmail.com>
2020-04-27 10:32:21 +0200

    38

---
+++ php-38/index.php
@@ -0,0 +1,32 @@
+<?php
+
+// Slå på all felrapportering. Bra under utveckling, dåligt i produktion.
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$fileName = '../content.txt';
+
+$fileHandle = fopen($fileName, 'r');
+
+$data = fread($fileHandle, filesize($fileName));
+
+fclose($fileHandle);
+
+?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo $data;
+    ?>
+</body>
+
+</html>
