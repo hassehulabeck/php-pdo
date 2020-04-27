ae7b7f012bf90393dbd7cb857816dd341daa90e7
hassehulabeck <hansbudandersson@gmail.com>
2020-04-27 10:39:47 +0200

    Used citation marks

---
+++ php-41/index.php
@@ -0,0 +1,32 @@
+<?php
+
+// Slå på all felrapportering. Bra under utveckling, dåligt i produktion.
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$fileName = '../content.txt';
+
+
+$data = file($fileName);
+
+
+?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    foreach ($data as $row) {
+        echo "<p>$row";
+    }
+    ?>
+</body>
+
+</html>
