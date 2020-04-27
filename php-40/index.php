9dbb78ba1eac3ff5edb6abe7048ca3b85df17abe
hassehulabeck <hansbudandersson@gmail.com>
2020-04-27 10:35:31 +0200

    removed fopen

---
+++ php-40/index.php
@@ -0,0 +1,31 @@
+<?php
+
+// Slå på all felrapportering. Bra under utveckling, dåligt i produktion.
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$data = '';
+$fileName = '../content.txt';
+
+
+$data = file_get_contents($fileName);
+
+
+?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo $data;
+    ?>
+</body>
+
+</html>
